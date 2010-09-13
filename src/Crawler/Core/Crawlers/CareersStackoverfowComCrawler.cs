79cd159c406e5c74fde0d5e1e7734b1c49eda374
alexander.beletsky <alexander.beletsky@gmail.com>
2010-09-13 19:00:28 +0300

    stub crawler for careers.stackoverflow is added

--- src/Crawler/Core/Crawlers/CareersStackoverfowComCrawler.cs
+++ src/Crawler/Core/Crawlers/CareersStackoverfowComCrawler.cs
@@ -5,23 +5,60 @@ using System.Text;
 
 namespace Crawler.Core.Crawlers
 {
-    public class CareersStackoverfowComCrawler : ICrawler
+    public class CareersStackoverfowComCrawler : CrawlerImpl, ICrawler
     {
-        private static readonly string _baseUrl = @"http://careers.stackoverflow.com";
-        private static readonly string _searchBaseUrl = @"http://careers.stackoverflow.com/Jobs?searchTerm=.net,java,c%2B%2B&searchType=Any&location=&range=20";
-        private IHtmlDocumentLoader _loader;
-        private ICrawlerRepository _context;
-        private ILogger _logger;
+        private string _baseUrl = @"http://careers.stackoverflow.com";
+        private string _searchBaseUrl = @"http://careers.stackoverflow.com/Jobs?searchTerm=.net,java,c%2B%2B&searchType=Any&location=&range=20";
 
         public CareersStackoverfowComCrawler(ILogger logger)
         {
-            _logger = logger;
+            Logger = logger;
         }
 
         public void Crawle(IHtmlDocumentLoader loader, ICrawlerRepository context)
         {
-            _loader = loader;
-            _context = context;
+            Loader = loader;
+            Repository = context;
+        }
+
+        protected override string BaseUrl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        protected override string SearchBaseUrl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        protected override IEnumerable<HtmlAgilityPack.HtmlNode> GetJobRows(HtmlAgilityPack.HtmlDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string CreateNextUrl(int nextPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetVacancyUrl(HtmlAgilityPack.HtmlNode row)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetVacancyBody(HtmlAgilityPack.HtmlDocument htmlDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetPosition(HtmlAgilityPack.HtmlNode row)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetCompany(HtmlAgilityPack.HtmlNode row)
+        {
+            throw new NotImplementedException();
         }
     }
 }
